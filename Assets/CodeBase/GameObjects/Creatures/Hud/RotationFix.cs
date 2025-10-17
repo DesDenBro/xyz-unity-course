a4d0f3e2985c5b6504d4a47ea07e852c94af5478
Denis Blinov <oceanik815@yandex.ru>
2025-10-17 14:34:14 +0300

    block 3 homework 16
    final hp bar

---
+++ Assets/CodeBase/GameObjects/Creatures/Hud/RotationFix.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace PixelCrew.GameObjects.Creatures.Hud
+{
+    public class RotationFix : MonoBehaviour
+    {
+        void FixedUpdate()
+        {
+            var parentTransform = gameObject.GetComponentInParent<Transform>();
+            var x = (parentTransform.lossyScale.x < 0 ? -1 : 1) * gameObject.transform.localScale.x;
+            gameObject.transform.localScale = new Vector3(x, gameObject.transform.localScale.y, gameObject.transform.localScale.z);
+        }
+    }
+}
