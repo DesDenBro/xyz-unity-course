c8db5198bb00f7a638efaf239aa04b68795300d4
Denis Blinov <oceanik815@yandex.ru>
2025-11-01 06:10:21 +0300

    block 3 homework 18
    dialog model

---
+++ Assets/CodeBase/Model/Definitions/DialogDef.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using PixelCrew.Model.Data;
+using UnityEngine;
+
+namespace PixelCrew.Model.Definitions
+{
+    [CreateAssetMenu(menuName = "Defs/Dialog", fileName = "Dialog")]
+    public class DialogDef : ScriptableObject
+    {
+        [SerializeField] private DialogData _data;
+
+        public DialogData Data => _data;
+    }
+}
---
+++ Assets/CodeBase/Utils/Editor/SerializedPropertyExtensions.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using UnityEditor;
+
+namespace PixelCrew.Utils.Editor
+{
+    public static class SerializedPropertyExtensions
+    {
+        public static bool GetEnum<TEnumType>(this SerializedProperty prop, out TEnumType enumType)
+            where TEnumType : Enum
+        {
+            enumType = default;
+            var names = prop.enumNames;
+
+            if (names == null || names.Length == 0) return false;
+
+            var enumName = names[prop.enumValueIndex];
+            enumType = (TEnumType)Enum.Parse(typeof(TEnumType), enumName);
+            return true;
+        }
+    }
+}
