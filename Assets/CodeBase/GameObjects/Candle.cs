533fb66854987157d1d9fbfc5b446c458dc3bf7b
Denis Blinov <oceanik815@yandex.ru>
2026-02-11 18:03:48 +0300

    block 4 homework 23
    candle final

--- Assets/CodeBase/GameObjects/Candle.cs
+++ Assets/CodeBase/GameObjects/Candle.cs
@@ -22,6 +22,7 @@ namespace PixelCrew.GameObjects
 
             _isActive = true;
             if (_current != null) StopCoroutine(_current);
+            gameObject.SetActive(true);
             _current = StartCoroutine(Action());
         }
 
@@ -30,30 +31,31 @@ namespace PixelCrew.GameObjects
             if (!IsActive) return;
 
             _isActive = false;
-            gameObject.SetActive(false);
             if (_current != null) StopCoroutine(_current);
+            gameObject.SetActive(false);
         }
 
         private IEnumerator Action()
         {
             var inv = GetComponentInParent<InventoryComponent>();
-            if (inv == null || inv.Count(InventoryItemName.Candle) == 0) yield break;
-
-            gameObject.SetActive(true);
-            do
+            if (inv != null && inv.Count(InventoryItemName.Candle) > 0)
             {
-                var count = inv.Count(InventoryItemName.Candle);
-                if (count == 0) break;
+                do
+                {
+                    var count = inv.Count(InventoryItemName.Candle);
+                    if (count == 0) break;
 
-                var finalRadius = _baseLightRadius * (count <= 10 ? count / 10 : 1);
-                var light = _light.GetComponent<Light2D>();
-                light.pointLightOuterRadius = finalRadius;
+                    var finalRadius = _baseLightRadius * (count <= 5 ? (float)count / 5 : 1);
+                    if (finalRadius < 0.5f) finalRadius = 0.5f;
+                    var light = _light.GetComponent<Light2D>();
+                    light.pointLightOuterRadius = finalRadius;
 
-                var success = inv.ChangeInventoryItemCount(InventoryItemName.Candle, -1);
-                if (success) break;
+                    var success = inv.ChangeInventoryItemCount(InventoryItemName.Candle, -1);
+                    if (!success) break;
 
-                yield return new WaitForSeconds(3);
-            } while (true);
+                    yield return new WaitForSeconds(5);
+                } while (true);
+            }
             
             TurnOff();
         }
